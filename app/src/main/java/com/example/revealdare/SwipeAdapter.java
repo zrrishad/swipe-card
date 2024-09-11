95085bb745884248890876791a0e848ed3660305
rishad <zrishad42@gmail.com>
2024-09-11 23:47:29 +0600

    swipe card

---
+++ app/src/main/java/com/example/revealdare/SwipeAdapter.java
@@ -0,0 +1,53 @@
+package com.example.revealdare;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.List;
+
+public class SwipeAdapter extends BaseAdapter {
+
+    private Context context;
+    private List<Integer>list;
+
+    public SwipeAdapter(Context context,List<Integer>list) {
+        this.context=context;
+        this.list=list;
+
+    }
+
+    @Override
+    public int getCount() {
+        return 20;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view;
+        if (convertView == null)
+        {
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_rishad,parent,false);
+        }else
+        {
+            view = convertView;
+        }
+
+
+
+
+        return view;
+    }
+}
